1a410ed6c3db9beccb5b6d331a29da4e6dd4f1ab
Pedro Lopes <pedromadeira201502@outlook.com>
2023-12-12 21:13:15 -0300

    feat: implement update endpoint

---
+++ src/queries/update-summarization.js
@@ -0,0 +1,23 @@
+import sql from "./index.js";
+
+const updateSummarization = async (id, data) => {
+  try {
+    const response = await sql` 
+        UPDATE summarizations SET 
+          title = ${data.title}, 
+          link = ${data.link}, 
+          startat = ${data.startAt}, 
+          endat = ${data.endat}, 
+          transcript = ${data.transcript},
+          summary = ${data.summary}
+        WHERE 
+          id = ${id};
+        `;
+    return response;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export default deleteSummarization;
