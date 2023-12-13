c5f8f343b7f5801e4f8d0ef17d13a8d086156216
Pedro Lopes <pedromadeira201502@outlook.com>
2023-12-12 21:12:28 -0300

    feat: use of the ValidateData class for better organization and reuse

---
+++ src/class/validatedData.js
@@ -0,0 +1,31 @@
+export class ValidateData{
+    constructor(title, link, startAt, endAt){
+        this.title = title;
+        this.link = link;
+        this.startAt = startAt;
+        this.endAt = endAt;
+    }
+
+updateSummary(summary){
+        if (typeof summary !== 'string')
+        throw 'Summary needs to be a string';
+    this.summary = summary;
+}
+
+updateTranscript(transcript){
+    if (typeof transcript !== 'string')
+            throw 'Text needs to be a string';
+        this.transcript = transcript;
+    }
+
+    isValidInfo(){
+        return (
+            typeof this.title === 'string' &&
+            typeof this.link === 'string' &&
+            typeof this.startAt === 'number' &&
+            typeof this.endAt === 'number' &&
+            typeof this.summary === 'string' &&
+            typeof this.transcript === 'string'
+        )
+    }
+}
